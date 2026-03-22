b2af2728187b117d424c01950bad3f58229f67e1
jkmmmm <127180604+jkmmmm@users.noreply.github.com>
2026-03-22 21:57:27 +0800

    Add files via upload

---
+++ scripts/biomedclip_cls.sh
@@ -0,0 +1,29 @@
+cd biomedclip_finetuning/open_clip/src
+
+## You can change the following parameters like the GPU devices, batch size, training data, epochs, and DHN-NCE loss parameters.
+
+CUDA_VISIBLE_DEVICES=0 python3 -m open_clip_train.main \
+    --batch-size 3000 \
+    --accum-freq 1 \
+    --workers 4 \
+    --report-to tensorboard \
+    --save-frequency 5 \
+    --logs="logs" \
+    --dataset-type csv \
+    --csv-separator="," \
+    --train-data /root/autodl-tmp/data/forensic_CT_train.csv \
+    --val-data /root/autodl-tmp/data/forensic_CT_valid.csv \
+    --csv-img-key image_path \
+    --csv-caption-key original_text \
+    --csv-disease-category category \
+    --csv-disease-location location \
+    --cls-model \
+    --config /root/autodl-tmp/data/forensic_CT_statistics.json \
+    --lr=1e-4 \
+    --wd=0.1 \
+    --warmup 10 \
+    --epochs=50 \
+    --model hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224 \
+    --pretrained /root/autodl-fs/clip_log/2025_10_17-00_47_54-model_hf-hub:microsoft-BiomedCLIP-PubMedBERT_256-vit_base_patch16_224-lr_0.0001-b_200-j_4-p_amp/checkpoints/epoch_latest.pt \
+    --save-best \
+    --delete-previous-checkpoint \
